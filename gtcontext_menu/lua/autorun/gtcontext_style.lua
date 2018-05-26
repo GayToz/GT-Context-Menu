5a1c9e2438fa563925b00bb9f388c3d9342f16ec
GayToz <39153774+GayToz@users.noreply.github.com>
2018-05-26 11:12:03 +0200

    Add files via upload

---
+++ gtcontext_menu/lua/autorun/gtcontext_style.lua
@@ -0,0 +1,64 @@
+/*------------------------------------------
+			ADDON BY GAYTOZ 
+------------------------------------------*/
+
+/*------------------------------------------
+			HOW TO ADD A STYLE ?
+
+	GTContext.style["..."] = {
+	color1 = Color(YOUR COLOR),  ----- COLOR OF THE BACKGROUND
+	color2 = Color(YOUR COLOR),  ----- THE BACKGROUND OF THE TEXT "INFORMATIONS", "COMMANDS" and THE NAM OF YOUR SERVER !
+	color3 = Color(YOUR COLOR),  ----- THE BACKGROUND OF THE BUTTONS
+    color4 = Color(YOUR COLOR),  ----- THE COLOR OF THE BUTTONS
+    color5 = Color(YOUR COLOR),  ----- THE BACKGROUND OF "Name", "Job"...
+    color6 = Color(YOUR COLOR)   ----- THE COLLOR OF THE SCROLL BAR
+    }
+
+------------------------------------------*/
+
+GTContext.style = {}
+
+GTContext.style["base"] = {
+	color1 = Color(0, 0, 30, 150),
+	color2 = Color(66, 66, 66, 255),
+	color3 = Color(50, 18, 50, 255), 
+    color4 = Color(66, 70, 80, 255),
+    color5 = Color(50, 50, 50, 150),
+    color6 = Color(0, 0, 0, 255)
+}
+
+GTContext.style["clair"] = {
+	color1 = Color(50, 150, 255, 255),
+	color2 = Color(50, 100, 150, 255),
+	color3 = Color(50, 120, 255, 255), 
+    color4 = Color(60, 130, 230, 255),
+    color5 = Color(0, 150, 255, 150),
+    color6 = Color(66, 66, 255, 255)
+}
+
+GTContext.style["red"] = {
+	color1 = Color(255, 0, 0, 150),
+	color2 = Color(150, 20, 0, 255),
+	color3 = Color(100, 30, 20, 255), 
+    color4 = Color(150, 70, 60, 255),
+    color5 = Color(250, 50, 50, 150),
+    color6 = Color(150, 0, 0, 255)
+}
+
+GTContext.style["blue"] = {
+	color1 = Color(0, 0, 255, 150),
+	color2 = Color(0, 20, 150, 255),
+	color3 = Color(20, 30, 100, 255), 
+    color4 = Color(60, 70, 150, 255),
+    color5 = Color(50, 50, 250, 150),
+    color6 = Color(0, 0, 150, 255)
+}
+
+GTContext.style["redandblue"] = {
+	color1 = Color(255, 0, 0, 150),
+	color2 = Color(0, 20, 255, 255),
+	color3 = Color(255, 150, 50, 255), 
+    color4 = Color(0, 0, 255, 255),
+    color5 = Color(0, 0, 255, 255),
+    color6 = Color(255, 0, 0, 255)
+}
